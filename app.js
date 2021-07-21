039c039e846223d255bf3eaa092daeec11ee774a
LucasFeli <felipe298@gmail.com>
2021-07-21 21:11:22 +0200

    adding error-handler mddw

--- app.js
+++ app.js
@@ -4,15 +4,21 @@ const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
 const app = express();
 require("dotenv").config();
+const notFound = require("./middleware/not-found")
+const errorHandlerMiddleware = require("./middleware/error-handler")
 
 
 // middleware
 app.use(express.static("./public"))
 app.use(express.json());
 
+
+
 //routes
 
 app.use("/api/v1/tasks", tasks);
+app.use(notFound);
+app.use(errorHandlerMiddleware)
 
 
 
